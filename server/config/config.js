0e3cd98dbc960446b82ab211b200f97c092d250c
Larry <ajacobozare@gmail.com>
2019-12-08 20:12:30 -0500

    RestServer - usuarios

--- server/config/config.js
+++ server/config/config.js
@@ -1,1 +1,21 @@
 process.env.PORT = process.env.PORT || 3000
+
+
+// ==================================
+// Entorno
+// ==================================
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
+
+let urlDB
+
+// ==================================
+// Base de datos
+// ==================================
+// if (process.env.NODE_ENV === 'dev') {
+//     urlDB = 'mongodb://localhost:27017/cafe'
+// } else {
+urlDB = 'mongodb+srv://cafe-user:123456A@cluster0-lliug.mongodb.net/cafe?retryWrites=true&w=majority'
+    // }
+
+process.env.URLDB = urlDB
