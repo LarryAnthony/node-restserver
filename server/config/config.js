a7d828d42ec812484caf482a0c31c882ffd6f9fe
Larry <ajacobozare@gmail.com>
2019-12-29 18:26:09 -0500

    Google sign in

--- server/config/config.js
+++ server/config/config.js
@@ -35,3 +35,9 @@ if (process.env.NODE_ENV === 'dev') {
 }
 
 process.env.URLDB = urlDB
+
+// ==================================
+// Google client ID
+// ==================================
+
+process.env.CLIENT_ID = process.env.CLIENT_ID || '440046911604-tr7ni6pspprtinhudb35htnj4rekcs9j.apps.googleusercontent.com'
